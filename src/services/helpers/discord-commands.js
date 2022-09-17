f88219870f84eaf28849524f09fe057ead85765c
DeafeningRoar <finalx7@gmail.com>
2022-09-17 17:46:27 -0300

    Refactor commands to use new db logic & include channel type

--- src/services/helpers/discord-commands.js
+++ src/services/helpers/discord-commands.js
@@ -1,4 +1,5 @@
-const { insertChannel, removeChannel, getChannel, removeChannels } = require('../../database');
+const Channels = require('../../database/channels');
+const channels = new Channels();
 
 const COMMANDS_LIST = {
   SET_CHANNEL: '/setchannel',
@@ -9,21 +10,60 @@ const COMMANDS_LIST = {
   REMOVE_ALERT: '/removealert'
 };
 
+const CHANNEL_TYPES = ['merchants', 'islands'];
+
 async function setChannelId(message) {
   try {
-    if (message.content !== COMMANDS_LIST.SET_CHANNEL || message.author.bot) {
+    if (!message.content.startsWith(COMMANDS_LIST.SET_CHANNEL) || message.author.bot) {
       return false;
     }
-    const [channel] = await getChannel({ channelId: message.channelId, guildId: message.guildId, isAlert: false });
+
+    const type = message.split(' ')[1];
+
+    if (!CHANNEL_TYPES.includes(type)) {
+      console.log(`Invalid channel type ${type} for channel ${message.channelId} in guild ${message.guildId}`);
+      await message.reply(`Invalid channel type. [${CHANNEL_TYPES.join(', ')}]`);
+      return false;
+    }
+
+    const [channel] = await channels.find([
+      {
+        key: 'channelId',
+        comparisonOperator: '=',
+        value: message.channelId
+      },
+      {
+        key: 'guildId',
+        comparisonOperator: '=',
+        value: message.guildId
+      },
+      {
+        key: 'type',
+        comparisonOperator: '=',
+        value: type
+      },
+      {
+        key: 'isAlert',
+        comparisonOperator: '=',
+        value: false
+      }
+    ]);
 
     if (channel) {
-      console.log(`Channel ${message.channelId} in guild ${message.guildId} already registered`);
-      await message.reply('Channel is already registered');
+      console.log(
+        `Channel with type ${type} in channel ${message.channelId} of guild ${message.guildId} already registered`
+      );
+      await message.reply(`Channel is already registered for ${type}`);
       return false;
     }
 
-    await insertChannel(message.channelId, message.guildId);
-    await message.reply('Using current channel');
+    await channels.insert({
+      channelId: message.channelId,
+      guildId: message.guildId,
+      isAlert: false,
+      type
+    });
+    await message.reply(`Using current channel for ${type}`);
     return true;
   } catch (error) {
     console.log('Set channel error', error);
@@ -33,12 +73,41 @@ async function setChannelId(message) {
 
 async function removeChannelId(message) {
   try {
-    if (message.content !== COMMANDS_LIST.REMOVE_CHANNEL || message.author.bot) {
+    if (!message.content.startsWith(COMMANDS_LIST.REMOVE_CHANNEL) || message.author.bot) {
       return false;
     }
 
-    await removeChannel(message.channelId, message.guildId, false);
-    await message.reply('Removed current channel');
+    const type = message.split(' ')[1];
+
+    if (!CHANNEL_TYPES.includes(type)) {
+      console.log(`Invalid channel type ${type} for channel ${message.channelId} in guild ${message.guildId}`);
+      await message.reply(`Invalid channel type. [${CHANNEL_TYPES.join(', ')}]`);
+      return false;
+    }
+
+    await channels.delete([
+      {
+        key: 'channelId',
+        comparisonOperator: '=',
+        value: message.channelId
+      },
+      {
+        key: 'guildId',
+        comparisonOperator: '=',
+        value: message.guildId
+      },
+      {
+        key: 'isAlert',
+        comparisonOperator: '=',
+        value: false
+      },
+      {
+        key: 'type',
+        comparisonOperator: '=',
+        value: type
+      }
+    ]);
+    await message.reply(`Removed current channel for ${type}`);
     return true;
   } catch (error) {
     console.log('Remove channel error', error);
@@ -56,12 +125,12 @@ async function clearChannels(message) {
       return false;
     }
 
-    let params;
+    let filters = [];
     if (message.content === COMMANDS_LIST.CLEAR_GUILD) {
-      params = { guildId: message.guildId };
+      filters.push({ key: 'guildId', comparisonOperator: '=', value: message.guildId });
     }
 
-    await removeChannels(params);
+    await channels.delete(filters);
     await message.reply('Removed channels');
     return true;
   } catch (error) {
@@ -75,7 +144,24 @@ async function setAlertChannel(message) {
     if (message.content !== COMMANDS_LIST.SET_ALERT_CHANNEL || message.author.bot) {
       return false;
     }
-    const [channel] = await getChannel({ channelId: message.channelId, guildId: message.guildId, isAlert: true });
+
+    const [channel] = await channels.find([
+      {
+        key: 'channelId',
+        comparisonOperator: '=',
+        value: message.channelId
+      },
+      {
+        key: 'guildId',
+        comparisonOperator: '=',
+        value: message.guildId
+      },
+      {
+        key: 'isAlert',
+        comparisonOperator: '=',
+        value: true
+      }
+    ]);
 
     if (channel) {
       console.log(`Alert Channel ${message.channelId} in guild ${message.guildId} already registered`);
@@ -83,7 +169,12 @@ async function setAlertChannel(message) {
       return false;
     }
 
-    await insertChannel(message.channelId, message.guildId, true);
+    await channels.insert({
+      channelId: message.channelId,
+      guildId: message.guildId,
+      isAlert: true,
+      type: 'alerts'
+    });
     await message.reply('Using current channel as alert');
     return true;
   } catch (error) {
@@ -98,7 +189,23 @@ async function removeAlertChannel(message) {
       return false;
     }
 
-    await removeChannel(message.channelId, message.guildId, true);
+    await channels.delete([
+      {
+        key: 'channelId',
+        comparisonOperator: '=',
+        value: message.channelId
+      },
+      {
+        key: 'guildId',
+        comparisonOperator: '=',
+        value: message.guildId
+      },
+      {
+        key: 'isAlert',
+        comparisonOperator: '=',
+        value: true
+      }
+    ]);
     await message.reply('Removed current channel');
     return true;
   } catch (error) {
@@ -106,6 +213,15 @@ async function removeAlertChannel(message) {
     return false;
   }
 }
+
+const COMMAND_HANDLERS = {
+  [COMMANDS_LIST.SET_CHANNEL]: setChannelId,
+  [COMMANDS_LIST.REMOVE_CHANNEL]: '',
+  [COMMANDS_LIST.CLEAR_GUILD]: '',
+  [COMMANDS_LIST.CLEAR_ALL]: '',
+  [COMMANDS_LIST.SET_ALERT_CHANNEL]: '',
+  [COMMANDS_LIST.REMOVE_ALERT]: ''
+};
 
 module.exports = {
   setChannelId,
