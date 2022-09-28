81c30a0d9f9cdea23ff57774b504dbd324eedcde
DeafeningRoar <finalx7@gmail.com>
2022-09-28 11:04:46 -0300

    Change server to gienah

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const channelsDB = new Channels();
 async function start() {
   try {
     const discord = new Discord();
-    const merchantsHub = new MerchantsHub({ server: 'Yorn' });
+    const merchantsHub = new MerchantsHub({ server: 'Gienah' });
     const islandsTracker = new IslandsTracker();
 
     discordHandlers({ discord, merchantsHub, islandsTracker });
