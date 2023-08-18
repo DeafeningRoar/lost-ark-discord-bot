f636d1af215ffd9e978e4edcb288e0d277ce3dea
DeafeningRoar <finalx7@gmail.com>
2023-08-18 02:10:25 -0400

    Fix log object object

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,15 +1,23 @@
+const { inspect } = require('util');
+
 function sleep(ms) {
   return new Promise(resolve => {
     setTimeout(resolve, ms);
   });
 }
 
-const formatError = (context, error) => ({
-  timestamp: new Date().toISOString(),
-  context,
-  message: error?.message || error,
-  stack: error?.stack
-});
+const formatError = (context, error) =>
+  inspect(
+    {
+      timestamp: new Date().toISOString(),
+      context,
+      message: error?.message || error,
+      stack: error?.stack
+    },
+    false,
+    null,
+    false
+  );
 
 module.exports = {
   sleep,
